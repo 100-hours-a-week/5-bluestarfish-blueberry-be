44e945ef82f0c973849461f51fb367a934ec8812
DohunHyun <dohun.hyun@gmail.com>
2024-08-20 19:51:34 +0900

    feat: Comment CRUD API

--- src/main/java/com/bluestarfish/blueberry/comment/repository/CommentRepository.java
+++ src/main/java/com/bluestarfish/blueberry/comment/repository/CommentRepository.java
@@ -1,7 +1,12 @@
 package com.bluestarfish.blueberry.comment.repository;
 
 import com.bluestarfish.blueberry.comment.entity.Comment;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findByPostIdAndDeletedAtIsNull(Long postId, Pageable pageable);
+    Optional<Comment> findByIdAndDeletedAtIsNull(Long commentId);
 }
