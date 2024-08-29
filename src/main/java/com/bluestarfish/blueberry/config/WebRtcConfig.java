dddcb0983a2548bbe0050a97a9021f9e8b4eaa8a
BenchPress200 <jms0538@naver.com>
2024-08-29 17:31:53 +0900

    chore

--- src/main/java/com/bluestarfish/blueberry/config/WebRtcConfig.java
+++ src/main/java/com/bluestarfish/blueberry/config/WebRtcConfig.java
@@ -5,12 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSocket
 public class WebRtcConfig implements WebSocketConfigurer {
 
     private final KurentoHandler kurentoHandler;
@@ -31,6 +33,6 @@ public class WebRtcConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(kurentoHandler, signalUrl)
-                .setAllowedOriginPatterns(frontendServerIp);
+                .setAllowedOriginPatterns("*");
     }
 }
