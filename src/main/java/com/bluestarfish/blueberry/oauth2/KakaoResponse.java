b4f0a9fe04d4f5f435cf89f2d9da6e4ef6d8a047
BenchPress200 <jms0538@naver.com>
2024-09-03 20:53:20 +0900

    fix: modify oauth2 login

--- src/main/java/com/bluestarfish/blueberry/oauth2/KakaoResponse.java
+++ src/main/java/com/bluestarfish/blueberry/oauth2/KakaoResponse.java
@@ -1,21 +1,19 @@
 package com.bluestarfish.blueberry.oauth2;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Map;
 
+@Slf4j
 public class KakaoResponse {
     private final Map<String, Object> attribute;
 
     public KakaoResponse(Map<String, Object> attribute) {
         this.attribute = (Map<String, Object>) attribute.get("kakao_account");
         this.attribute.put("id", attribute.get("id"));
-
     }
 
-    public String getProviderId() {
-        return attribute.get("id").toString();
-    }
-    
     public String getEmail() {
-        return attribute.get("email").toString();
+        return attribute.get("email").toString() + "@" + attribute.get("id").toString();
     }
 }
