5252bd9e38596ef1a64f2aa4215b9203051564b6
DohunHyun <dohun.hyun@gmail.com>
2024-09-19 20:11:49 +0900

    fix #216: Add Valid to Room Request

--- src/main/java/com/bluestarfish/blueberry/room/dto/RoomRequest.java
+++ src/main/java/com/bluestarfish/blueberry/room/dto/RoomRequest.java
@@ -1,6 +1,8 @@
 package com.bluestarfish.blueberry.room.dto;
 
 import com.bluestarfish.blueberry.room.entity.Room;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,10 +14,18 @@ import org.springframework.web.multipart.MultipartFile;
 public class RoomRequest {
     private Long id;
     private Long userId;
+
+    @NotBlank(message = "Title must not be blank")
+    @Size(min = 2, max = 15, message = "Title must be between 5 and 20 characters")
     private String title;
+
     private int maxUsers;
     private boolean isCamEnabled;
+
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 4, max = 20, message = "Password must be between 5 and 10 characters")
     private String password;
+
     private MultipartFile thumbnail;
     private String description;
 
